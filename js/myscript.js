9ea3e1880211924cdc25e8c0662747ebd481c909
BaccanoB <beatrice.baccano@yahoo.it>
2021-04-26 15:15:16 +0200

    inseriti ;

--- js/myscript.js
+++ js/myscript.js
@@ -1,12 +1,12 @@
-var nome = prompt("Inserisci il tuo nome")
-console.log(nome)
+var nome = prompt("Inserisci il tuo nome");
+console.log(nome);
 
-var cognome = prompt("Inserisci il tuo cognome")
-console.log(cognome)
+var cognome = prompt("Inserisci il tuo cognome");
+console.log(cognome);
 
-var colore = prompt("Inserisci il tuo colore preferito")
-console.log(colore)
+var colore = prompt("Inserisci il tuo colore preferito");
+console.log(colore);
 
-document.getElementById("nome").innerHTML = nome + "!"
+document.getElementById("nome").innerHTML = nome + "!";
 
-document.getElementById("password").innerHTML = nome + cognome + colore + "21"
+document.getElementById("password").innerHTML = nome + cognome + colore + "21";
